fa09dd49b1af40471bff68ec78b9cbd35c05bef2
Roza <rozaogannesyan109@gmail.com>
2023-10-05 16:17:48 +0300

    карты

--- local/templates/ruki-template/footer.php
+++ local/templates/ruki-template/footer.php
@@ -14,6 +14,197 @@ use Bitrix\Main\UI\Extension;
 
 
 
+<? if (CSite::InDir('/contacts/')) : ?>
+
+	<div class="locations">
+		<div class="map" id="map"></div>
+	</div>
+
+	<script src="https://api-maps.yandex.ru/2.0-stable/?apikey=ваш API-ключ&?apikey=ваш API-ключ&load=package.standard&lang=ru-RU"></script>
+	<script>
+		window.addEventListener('DOMContentLoaded', () => {
+
+			function init() {
+				let map = new ymaps.Map("map", {
+					center: [43.04545166274155, 44.67198985350388], // ваши данные
+					zoom: 13,
+				});
+
+				let placemark = new ymaps.Placemark(
+					[43.03158257448738, 44.68968499999997], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Ватутина, 51, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				let placemark4 = new ymaps.Placemark(
+					[43.05881357450824, 44.66508949999998], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Пожарского, 5, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				let placemark2 = new ymaps.Placemark(
+					[43.03309857449133, 44.68330699999999], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Джанаева, 37, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+
+				let placemark3 = new ymaps.Placemark(
+					[43.04864857448196, 44.632740999999946], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">Владикавказская ул., 26, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				map.controls.remove("geolocationControl"); // удаляем геолокацию
+				map.controls.remove("searchControl"); // удаляем поиск
+				map.controls.remove("trafficControl"); // удаляем контроль трафика
+				map.controls.remove("typeSelector"); // удаляем тип
+				map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+				map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+				map.controls.remove("rulerControl"); // удаляем контрол правил
+				map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+				map.geoObjects.add(placemark);
+				map.geoObjects.add(placemark2);
+				map.geoObjects.add(placemark3);
+				map.geoObjects.add(placemark4);
+			}
+
+			ymaps.ready(init);
+		})
+	</script>
+
+
+
+<? endif; ?>
+
+
+<? if (CSite::InDir('/studio/')) : ?>
+	<script src="https://api-maps.yandex.ru/2.0-stable/?apikey=ваш API-ключ&?apikey=ваш API-ключ&load=package.standard&lang=ru-RU"></script>
+	<script>
+		window.addEventListener('DOMContentLoaded', () => {
+			document.querySelectorAll("#lightgallery").forEach((element) => {
+				lightGallery(element, {
+					plugins: [lgZoom, lgThumbnail],
+					licenseKey: "your_license_key",
+					speed: 500,
+					// ... other settings
+				});
+			});
+
+			function init() {
+				let map = new ymaps.Map("map", {
+					center: [43.04545166274155, 44.67198985350388], // ваши данные
+					zoom: 13,
+				});
+
+				let placemark = new ymaps.Placemark(
+					[43.03158257448738, 44.68968499999997], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Ватутина, 51, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				let placemark4 = new ymaps.Placemark(
+					[43.05881357450824, 44.66508949999998], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Пожарского, 5, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				let placemark2 = new ymaps.Placemark(
+					[43.03309857449133, 44.68330699999999], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">ул. Джанаева, 37, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+
+				let placemark3 = new ymaps.Placemark(
+					[43.04864857448196, 44.632740999999946], {
+						balloonContent: `
+      <div class="balloon">
+        <div class="balloon__address">Владикавказская ул., 26, Владикавказ</div>
+      </div>
+    `,
+					}, {
+						iconLayout: "default#image",
+						iconImageHref: "<?php echo SITE_TEMPLATE_PATH ?>/assets/images/icon-gps-map.png",
+						iconImageSize: [56, 56],
+						iconImageOffset: [-25, -50],
+					}
+				);
+				map.controls.remove("geolocationControl"); // удаляем геолокацию
+				map.controls.remove("searchControl"); // удаляем поиск
+				map.controls.remove("trafficControl"); // удаляем контроль трафика
+				map.controls.remove("typeSelector"); // удаляем тип
+				map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+				map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+				map.controls.remove("rulerControl"); // удаляем контрол правил
+				map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+				map.geoObjects.add(placemark);
+				map.geoObjects.add(placemark2);
+				map.geoObjects.add(placemark3);
+				map.geoObjects.add(placemark4);
+			}
+
+			ymaps.ready(init);
+		})
+	</script>
+<? endif; ?>
+
 <? if ($APPLICATION->GetCurPage(false) !== '/') : ?>
 	</div>
 <? endif; ?>
